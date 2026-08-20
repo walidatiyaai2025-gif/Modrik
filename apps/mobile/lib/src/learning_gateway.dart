da203fbea8b3dea76986c722aa0dab4e386e7391
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-20 22:02:14 +0300

    style(mobile-auth): satisfy learning analyzer rules

--- apps/mobile/lib/src/learning_gateway.dart
+++ apps/mobile/lib/src/learning_gateway.dart
@@ -131,23 +131,22 @@ class HttpLearningGateway implements LearningGateway {
   HttpLearningGateway({
     required this.baseUrl,
     String? bearerToken,
-    String? Function()? bearerTokenProvider,
+    this.bearerTokenProvider,
     this.onAuthenticationRejected,
     this.onEmailVerificationRequired,
     HttpClient? client,
   })  : _staticBearerToken = bearerToken,
-        _bearerTokenProvider = bearerTokenProvider,
         _client = client ?? HttpClient();
 
   final Uri baseUrl;
   final String? _staticBearerToken;
-  final String? Function()? _bearerTokenProvider;
+  final String? Function()? bearerTokenProvider;
   final void Function()? onAuthenticationRejected;
   final void Function()? onEmailVerificationRequired;
   final HttpClient _client;
 
   String? get _bearerToken =>
-      _bearerTokenProvider?.call() ?? _staticBearerToken;
+      bearerTokenProvider?.call() ?? _staticBearerToken;
 
   @override
   Future<Session> session() async =>
