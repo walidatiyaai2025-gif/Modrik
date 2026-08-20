48c0fd72449d51528b62633a60ae22d20aa42956
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-21 02:51:11 +0300

    style(obs): restore Pint-clean bootstrap formatting

--- apps/backend/bootstrap/app.php
+++ apps/backend/bootstrap/app.php
@@ -16,13 +16,12 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
         apiPrefix: '',
     )
