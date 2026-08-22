f7b41bf7d289fa1c6862c641e29ff5f657e51a74
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-22 20:10:19 +0300

    Web: mark Student Portal runtime route

--- apps/web/src/app/student/page.tsx
+++ apps/web/src/app/student/page.tsx
@@ -4,6 +4,7 @@ import NotificationShortcut from "./notification-shortcut";
 export default function StudentPortalPage() {
   return (
     <>
+      <span data-testid="modrik-student-portal-route" data-portal="student" hidden />
       <AuthWorkspace />
       <NotificationShortcut />
     </>
