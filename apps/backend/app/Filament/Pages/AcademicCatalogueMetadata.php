186b588d56af5e2d15d6578615cc218e1cc9befb
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-29 18:12:42 +0300

    style: satisfy Pint class attribute separation

--- apps/backend/app/Filament/Pages/AcademicCatalogueMetadata.php
+++ apps/backend/app/Filament/Pages/AcademicCatalogueMetadata.php
@@ -19,12 +19,17 @@ final class AcademicCatalogueMetadata extends Page
     protected static string|UnitEnum|null $navigationGroup = AdminNavigationGroup::Academic;
 
     public ?string $yearLevel = null;
+
     public string $yearLabelAr = '';
+
     public string $yearLabelEn = '';
+
     public string $yearLabelFr = '';
+
     public int $yearDisplayOrder = 0;
 
     public ?string $trackId = null;
+
     public int $trackDisplayOrder = 0;
 
     public static function canAccess(): bool
