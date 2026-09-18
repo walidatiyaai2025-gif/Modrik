660cfff01a8483f716ee60161b1484f9b01f3687
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-18 10:14:41 +0300

    test(#354): account for Workbench supporting surface

--- apps/backend/tests/Feature/AdminContentOperationsTest.php
+++ apps/backend/tests/Feature/AdminContentOperationsTest.php
@@ -222,7 +222,7 @@ class AdminContentOperationsTest extends TestCase
         $this->assertSame(['required', 'active', 'active', 'gate', 'gate'], array_column($steps, 'state'));
         $this->assertNotContains('', array_column($steps, 'url'));
         $supporting = $operations->supportingSurfaces();
-        $this->assertCount(3, $supporting);
+        $this->assertCount(4, $supporting);
         $this->assertNotContains('', array_column($supporting, 'url'));
         foreach ($operations->deferredCapabilities() as $capability) {
             $this->assertSame('deferred_disabled', $capability['classification']);
