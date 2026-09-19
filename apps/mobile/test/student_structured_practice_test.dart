1cb212ce2ceac2c844ad675516d96fdbc8ff1211
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-09-19 23:42:20 +0300

    test(#358): remove unused Flutter test import

--- apps/mobile/test/student_structured_practice_test.dart
+++ apps/mobile/test/student_structured_practice_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:modrik_mobile/main.dart';
 import 'package:modrik_mobile/src/learning_gateway.dart';
